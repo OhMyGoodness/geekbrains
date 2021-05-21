546e3275ee558622341a316ec9fc78f3544e2753
Danil Lazin <xelnagaaa@gmail.com>
2021-05-21 17:59:08 +0700

    lesson1

--- lesson1-2.playground/Contents.swift
+++ lesson1-2.playground/Contents.swift
@@ -1,13 +1,12 @@
 import Foundation
 
-let a:Int = 2
-let b:Int = 5
+let a: Int = 2
+let b: Int = 5
 
 // площадь по 2м катетам
-let s:Double = Double(a) * Double(b) / 2
+let s: Double = Double(a) * Double(b) / 2
 
 print("Площадь треугольника = " + String(s))
 
-
-let perrimeter:Double = (sqrt(pow(Double(a),2) + pow(Double(b),2) + (Double(a + b))))
+let perrimeter: Double = (sqrt(pow(Double(a), 2) + pow(Double(b), 2) + (Double(a + b))))
 print("Периметр треугольника: " + String(perrimeter))
--- lesson1-3.playground/Contents.swift
+++ lesson1-3.playground/Contents.swift
@@ -1,23 +1,21 @@
 import Foundation
 
-let contribYears:Int = 5
+let contribYears: Int = 5
 
 print("Введите сумму вклада: ")
-let inputSumm:String? = readLine()
-if (inputSumm != nil)
-{
+let inputSumm: String? = readLine()
+if (inputSumm != nil) {
     print("Введите годовой процент: ")
-    let percentInput:String? = readLine()
-    
-    if (percentInput != nil){
-        let percent:Double = Double(percentInput!)! / 100
-        
-        var totalSumm:Double = Double(inputSumm!)!
-        for index in 1...5
-        {
-            totalSumm += (totalSumm*percent)
+    let percentInput: String? = readLine()
+
+    if (percentInput != nil) {
+        let percent: Double = Double(percentInput!)! / 100
+
+        var totalSumm: Double = Double(inputSumm!)!
+        for index in 1...5 {
+            totalSumm += (totalSumm * percent)
         }
-        
+
         print("Сумма вклада = " + inputSumm! + ", годовой процент: " + percentInput! +
                 ", сумма вкалада через " + String(contribYears) + " лет = " + String(format: "%.00f", totalSumm))
     }
