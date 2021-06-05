eb14748207780da48cfc71e7fbc5f35846476930
xelnagaaa@gmail.com <01021983>
2021-06-05 15:29:24 +0700

    lesson5

--- lessons/main.swift
+++ lessons/main.swift
@@ -1,189 +1,120 @@
 //
 //  main.swift
-//  lesson4
+//  lesson5
 //
-//  Created by Данила Лазин on 27.05.2021.
+//  Created by Данила Лазин on 04.06.2021.
 //
+//1. Создать протокол «Car» и описать свойства, общие для автомобилей, а также метод действия.
+//2. Создать расширения для протокола «Car» и реализовать в них методы конкретных действий с автомобилем:
+//   открыть/закрыть окно, запустить/заглушить двигатель и т.д.
+//   (по одному методу на действие, реализовывать следует только те действия, реализация которых общая для всех автомобилей).
+//3. Создать два класса, имплементирующих протокол «Car» - trunkCar и sportСar. Описать в них свойства,
+//   отличающиеся для спортивного автомобиля и цистерны.
+//4. Для каждого класса написать расширение, имплементирующее протокол CustomStringConvertible.
+//5. Создать несколько объектов каждого класса. Применить к ним различные действия.
+//6. Вывести сами объекты в консоль.
 //
 
 import Foundation
 
-class Car {
-    var CarClass: CarClassType?
+enum MyCustomCargoException: Error {
+    case cargo_not_initialized
+}
 
-    var YearProduction: Int?
+protocol CarProtocol {
+    var YearProduction: Int { get set }
+    var Model: String { get set }
+    var GasVolume: Float { get set }
 
-    var CarModel: String?
+    var IsWindowOpened: Bool { get set }
+    var IsEngineRunned: Bool { get set }
 
-    var isEngineStarted: Bool = false
+    init(_ yearProd: Int, _ model: String, _ gasVolume: Float)
 
-    var isWindowOpened: Bool = false
+    func getCurrentGasVolume() -> Float
+}
 
-    var MyCargo: CargoType = .UNKNOWN
-
-    var CargoCurrentLoad: Float = 0
-
-    var CargoMaxLoad: Float = 0
-
-    init(yearProd: Int, model: String) {
-        YearProduction = yearProd
-        CarModel = model
+extension CarProtocol {
+    mutating func onActionWindowOpen() -> () {
+        self.IsWindowOpened = true
+        print("Действие: открыть окна")
     }
 
-    enum CarClassType: String {
-        case SPORT = "Спортивный"
-        case TRUNK = "Грузовой"
+    mutating func onActionWindowClose() -> () {
+        self.IsWindowOpened = false
+        print("Действие: закрыть окна")
     }
 
-    enum CargoType {
-        case UNKNOWN
-        case BODY
-        case TRUNK
+    mutating func onEngineRun() -> () {
+        IsEngineRunned = true
+        print("Действие: запустить двигатель")
     }
 
-    enum CarActionType {
-        case RUN_ENGINE
-        case STOP_ENGINE
-        case OPEN_WINDOWS
-        case CLOSE_WINDOWS
-        case UPLOAD_CARGO(volume: Float)
-        case UNLOAD_CARGO(volume: Float)
-    }
-
-    func onAction(action: CarActionType) {
-        switch action {
-        case .RUN_ENGINE:
-            isEngineStarted = true
-            print("Двигатель успешно запущен")
-        case .STOP_ENGINE:
-            isEngineStarted = false
-            print("Двигатель успешно остановлен")
-        case .OPEN_WINDOWS:
-            isWindowOpened = true
-            print("Окна успешно открыты")
-        case .CLOSE_WINDOWS:
-            isWindowOpened = false
-            print("Окна успешно закрыты")
-        default:
-            break
-        }
-    }
-
-    final func toString() -> String {
-        var cargoClassString: String = "Неизвестно"
-        switch MyCargo {
-        case .BODY:
-            cargoClassString = "Кузов"
-        case .TRUNK:
-            cargoClassString = "Багажник"
-        default:
-            break
-        }
-        return "Модель: \(CarModel!), Год выпуска: \(YearProduction!), Класс авто: \(CarClass!.rawValue), Тип грузового отсека: \(cargoClassString), Максимальный объем груза: \(CargoMaxLoad)"
-    }
-
-    final func actionsToString() -> String{
-        return "Двигатель: \((isEngineStarted ? "запущен" : "остановлен")), Окна: \((isWindowOpened ? "открыты" : "закрыты"))"
+    mutating func onEngineStop() -> () {
+        IsEngineRunned = true
+        print("Действие: остановить двигатель")
     }
 }
 
-class SportCar: Car {
-    override init(yearProd: Int, model: String) {
-        super.init(yearProd: yearProd, model: model)
-        CarClass = .SPORT
-        MyCargo = .BODY
-    }
-
-    func initCargoSettings(_ maxVolume: Float) {
-        CargoMaxLoad = maxVolume
-    }
-
-    override func onAction(action: CarActionType) {
-        super.onAction(action: action)
-        switch action {
-
-        case .UPLOAD_CARGO(volume: let vol):
-            let checkCargo = CargoCurrentLoad + vol >= CargoMaxLoad
-            if (!checkCargo){
-                CargoCurrentLoad += vol
-                print("Загружено \(vol), текущий общий объем груза = \(CargoCurrentLoad)")
-            } else {
-                print("Невозможно выполнить загрузку, объем первышает максимальный")
-            }
-            break
-        case .UNLOAD_CARGO(volume: let vol):
-            let checkCargo = CargoCurrentLoad - vol < 0
-            if (!checkCargo){
-                CargoCurrentLoad -= vol
-                print("Выгружено \(vol), текущий общий объем груза = \(CargoCurrentLoad)")
-            } else {
-                print("Невозможно выполнить разгрузку, указанного объема не существует")
-            }
-            break;
-        default:
-            break
-        }
-    }
-
-}
-class TrunkCar: Car {
-    override init(yearProd: Int, model: String) {
-        super.init(yearProd: yearProd, model: model)
-        CarClass = .TRUNK
-        MyCargo = .TRUNK
-    }
-
-    func initCargoSettings(_ maxVolume: Float) {
-        CargoMaxLoad = maxVolume
-    }
-
-    override func onAction(action: CarActionType) {
-        super.onAction(action: action)
-        switch action {
-
-        case .UPLOAD_CARGO(volume: let vol):
-            let checkCargo = CargoCurrentLoad + vol >= CargoMaxLoad
-            if (!checkCargo){
-                CargoCurrentLoad += vol
-                print("Загружено \(vol), текущий общий объем груза = \(CargoCurrentLoad)")
-            } else {
-                print("Невозможно выполнить загрузку, объем первышает максимальный")
-            }
-            break
-        case .UNLOAD_CARGO(volume: let vol):
-            let checkCargo = CargoCurrentLoad - vol < 0
-            if (!checkCargo){
-                CargoCurrentLoad -= vol
-                print("Выгружено \(vol), текущий общий объем груза = \(CargoCurrentLoad)")
-            } else {
-                print("Невозможно выполнить разгрузку, указанного объема не существует")
-            }
-            break;
-        default:
-            break
-        }
-    }
-
+// простите, у меня травма)) перечисления привык в верхнем регистре делать((
+enum CargoType {
+    case BODY
+    case TRUNK
+    case GAS
 }
 
-var mersCar = SportCar(yearProd: 2021, model: "Mercedes 2021")
-mersCar.initCargoSettings(5)
-print(mersCar.toString())
-print(mersCar.actionsToString())
+struct CargoProperties {
+    var CargoMaxVolume: Float = 0.0
+    var CargoCurrentVolume: Float = 0
+    var CargoType: CargoType
 
-mersCar.onAction(action: .OPEN_WINDOWS)
-mersCar.onAction(action: .UPLOAD_CARGO(volume: 3))
-mersCar.onAction(action: .UPLOAD_CARGO(volume: 1))
-mersCar.onAction(action: .UNLOAD_CARGO(volume: 1))
-mersCar.onAction(action: .UNLOAD_CARGO(volume: 500))
+    init(_ maxVol: Float, _ type: CargoType) {
+        CargoMaxVolume = maxVol
+        CargoType = type
+        CargoCurrentVolume = 0
+    }
+}
 
-var teslaCar = TrunkCar(yearProd: 2121, model: "Tesla cyber Truck")
-teslaCar.initCargoSettings(1000)
-print(teslaCar.toString())
-print(teslaCar.actionsToString())
+protocol CarCargoProtocol {
+    var CargoProps: CargoProperties? { get set }
 
-teslaCar.onAction(action: .RUN_ENGINE)
-teslaCar.onAction(action: .UPLOAD_CARGO(volume: 34))
-teslaCar.onAction(action: .UPLOAD_CARGO(volume: 34))
-teslaCar.onAction(action: .UNLOAD_CARGO(volume: 34))
-teslaCar.onAction(action: .UNLOAD_CARGO(volume: 500))
+    var CargoMaxVolume: Float { get set }
+
+    var CargoInitialized: Bool { get set }
+
+    func InitializeCargo(_ maxVol: Float, _ type: CargoType)
+
+    mutating func UploadToCargo(_ volume: Float) -> ()
+
+    mutating func UnloadFromCargo(_ volume: Float) -> ()
+}
+
+var teslaCar = TeslaTrunkClass.init(2021, "X6", 500.5)
+teslaCar.UploadToCargo(10)
+teslaCar.InitializeCargo(1000, .TRUNK)
+teslaCar.UploadToCargo(100)
+teslaCar.onActionWindowOpen()
+
+if (teslaCar is CarNitroExtension)
+{
+    (teslaCar as! CarNitroExtension).onNitro()
+}
+
+print(teslaCar)
+
+var ferrariClass = FerrariClass.init(2022, "911", 50)
+ferrariClass.UploadToCargo(20)
+ferrariClass.InitializeCargo(30, .BODY)
+ferrariClass.UploadToCargo(10)
+
+if (ferrariClass is CarNitroExtension)
+{
+    (ferrariClass as CarNitroExtension).onNitro()
+}
+
+ferrariClass.onEngineRun()
+
+if (ferrariClass is CarNitroExtension)
+{
+    (ferrariClass as CarNitroExtension).onNitro()
+}
